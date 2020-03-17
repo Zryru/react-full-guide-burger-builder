26341a1388b5d53757e139e6a64dd3b4720310bd
Jorge Del Castillo <jorge.ale91@gmail.com>
2020-03-18 00:53:12 +0100

    Build controls

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,16 +1,33 @@
-import React from 'react'
-import classes from './Burger.module.css';
-import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+import React from "react";
+import classes from "./Burger.module.css";
+import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
-const Burger = (props) => {
-    return (
-        <div className={classes.Burger}>
-            <BurgerIngredient type="bread-top"></BurgerIngredient>
-            <BurgerIngredient type="cheese"></BurgerIngredient>
-            <BurgerIngredient type="meat"></BurgerIngredient>
-            <BurgerIngredient type="bread-bottom"></BurgerIngredient>
-        </div>
-    )
-}
+const Burger = props => {
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return (
+          <BurgerIngredient key={igKey + i} type={igKey}></BurgerIngredient>
+        );
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
 
-export default Burger
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients</p>;
+  }
+
+  console.log(transformedIngredients);
+
+  return (
+    <div className={classes.Burger}>
+      <BurgerIngredient type="bread-top"></BurgerIngredient>
+      {transformedIngredients}
+      <BurgerIngredient type="bread-bottom"></BurgerIngredient>
+    </div>
+  );
+};
+
+export default Burger;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,15 +1,25 @@
-import React, { Component } from 'react'
-import Burger from '../../components/Burger/Burger';
+import React, { Component } from "react";
+import Burger from "../../components/Burger/Burger";
+import BuildControls from "../../components/Burger/BuildConstrols/BuildControls";
 
 export class BurgerBuilder extends Component {
-    render() {
-        return (
-            <React.Fragment>
-                <Burger></Burger>
-                <div>Build Controls</div>
-            </React.Fragment>
-        )
+  state = {
+    ingredients: {
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0
     }
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <Burger ingredients={this.state.ingredients}></Burger>
+        <BuildControls></BuildControls>
+      </React.Fragment>
+    );
+  }
 }
 
 export default BurgerBuilder;
